cf1d22779c3acb411782cfe7c28686d8949bda2e
Ashley DelVentura <ashley@delventura.com>
2019-01-24 17:20:12 -0500

    Person2 Changes

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -3,7 +3,7 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -30,18 +30,28 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+		//Person 2 put your implementation here
+		StringBuilder inputSB = new StringBuilder(input);
+
+		for (int i = 0; i < input.length(); i++) {
+			inputSB.setCharAt(i, input.charAt((int) (Math.random() * input.length())));
+		}
+	  return inputSB.toString();
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
+	}
+
+	public static void main(String[] args) {
+		Person2 me = new Person2("Ashley");
+		System.out.println(me.calc("Ashley"));
 	}
 }
